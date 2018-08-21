6abe8396539cd68e28e89dd67710900d4bf90b38
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-08-21 14:29:31 +1200

    upload 3

---
+++ High Flying/Assets/Wingsuiting/Extensions/Vector3Extension.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector3Extension
+{
+	public static Vector3 ToLocalPosition(this Vector3 wordPosition, Transform parent)
+	{
+		Vector3 localPosition = wordPosition - parent.position;
+		return new Vector3( Vector3.Dot(localPosition, parent.right),
+			Vector3.Dot(localPosition, parent.up),
+			Vector3.Dot(localPosition, parent.forward)
+		);
+	}
+	public static Vector3 ToLocalPosition(this Vector3 wordPosition, Vector3 parentPosition, Transform parent)
+	{
+		Vector3 localPosition = wordPosition - parentPosition;
+		return new Vector3( Vector3.Dot(localPosition, parent.right),
+			Vector3.Dot(localPosition,parent.up),
+			Vector3.Dot(localPosition, parent.forward)
+		);
+	}
+
+	public static Vector3 ToWordPosition(this Vector3 localPosition, Transform parent)
+	{
+		return parent.position + localPosition.x*parent.right + localPosition.y*parent.up + localPosition.z*parent.forward;
+	}
+	public static bool SphereInCube (this Vector3 point, float radius,  Transform cube)
+	{
+		Vector3 localPos = point.ToLocalPosition(cube);
+		Vector3 cubeSqale = cube.lossyScale;
+		return Mathf.Abs(localPos.x) + radius <= 0.5f*cubeSqale.x && Mathf.Abs(localPos.y) + radius <= 0.5f*cubeSqale.y && Mathf.Abs(localPos.z) + radius <= 0.5f*cubeSqale.z;
+	}
+	public static bool SphereHitCone(this Vector3 spherPosition, float sphereRadius, Vector3 conePosition, Vector3 coneForward, float coneAngleRadian, float coneH)
+	{
+		Vector3 r = spherPosition - conePosition;
+		Vector3 p1 = Vector3.Project(r, coneForward);
+		if (p1.magnitude - sphereRadius > coneH || (Vector3.Dot(p1, coneForward) <= 0.0f && p1.magnitude > sphereRadius))
+		{
+			return false;
+		}
+		float distance1 = p1.magnitude * Mathf.Tan(0.5f * coneAngleRadian);
+		float distance2 = sphereRadius / Mathf.Cos(0.5f * coneAngleRadian);
+		Vector3 p2 = Vector3.ProjectOnPlane(r, coneForward);
+		return p2.magnitude <= distance1 + distance2;
+	}
+}
---
+++ High Flying/Assets/Wingsuiting/SimpleScripts/GradientTool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public delegate void GradientToolSelectColorHandler(Color color);
+
+public class GradientTool : MonoBehaviour
+{
+    public static event GradientToolSelectColorHandler OnSelectColor;
+
+    [SerializeField] private Slider colorGradient;
+    [SerializeField] private Slider colorLerp;
+    [SerializeField] private Texture2D colorGradientTexture;
+    [SerializeField]  private float maxWhite = 1.0f;
+
+    private float lerp;
+    private static Color color;
+
+    void Awake()
+    {
+        lerp = 0.0f;
+        colorGradient.onValueChanged.AddListener(OnColorChanged);
+        colorLerp.onValueChanged.AddListener(OnColorLerp);
+    }
+    public static void SetStartColor(Color value)
+    {
+        color = value;
+    }
+    void OnColorChanged(float value)
+    {
+        if (OnSelectColor != null)
+        {
+            color = colorGradientTexture.GetPixelBilinear(value, 0.0f);
+            OnSelectColor(Color.Lerp(color, Color.white, lerp));
+        }
+    }
+    void OnColorLerp(float value)
+    {
+        if (OnSelectColor != null)
+        {
+            lerp = Mathf.Clamp01(value * maxWhite);
+            OnSelectColor(Color.Lerp(color, Color.white, lerp));
+        }
+    }
+}
