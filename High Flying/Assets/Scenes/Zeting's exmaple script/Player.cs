1bf9ade91fcea4873a286a8ed8427774558cc799
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-08-28 13:44:11 +1200

    Comment has beed add to the control

--- High Flying/Assets/Scenes/Zeting's exmaple script/Player.cs
+++ High Flying/Assets/Scenes/Zeting's exmaple script/Player.cs
@@ -6,49 +6,54 @@ public class Player : MonoBehaviour
 {
 
     // Use this for initialization
-    [Tooltip("M/s")] [SerializeField] float moveSpeed = 8;
-    [SerializeField] float positionPitchFactor = -5f; 
-    [SerializeField] float controlPitchFactor = -20f; 
-    [SerializeField] float positionYawFactor = 5f; 
-    [SerializeField] float controlRollFactor = -30f; 
+    [Tooltip("M/s")] [SerializeField] float moveSpeed = 8; // move speed of the character to move
+    [SerializeField] float positionPitchFactor = -5f; // how much character can pitch dur by its position 
+    [SerializeField] float controlPitchFactor = -20f; // how much character can pitch dur by control 
+    [SerializeField] float positionYawFactor = 5f; // how much chararacter can yaw
+    [SerializeField] float controlRollFactor = -30f; //how much chararacter can roll
    
     
-    
-    
+
     float xThrow;
     float yThrow;
-    //Rigidbody rigidbody;
+  
     void Start()
     {
-        // this.rigidbody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
-        //fun();
-        rotate();
+        Move();// move the character left right up down
+      
+        rotate(); //  rotate the chatacter whey moving
     }
     private void rotate()
     {
-        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
-        float pitchDueToControlThrow = yThrow * controlPitchFactor;
-        float pitch = (pitchDueToPosition + pitchDueToControlThrow)/1.5f;
-        float yaw = transform.localPosition.x * positionYawFactor;
-        float roll = xThrow * controlRollFactor;
-        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;//different position when you move in Y asix
+
+        float pitchDueToControlThrow = yThrow * controlPitchFactor; // y Throw is the joysick (if keyboard is a line) how far from the origin
+                                                                    //  point in y asix
+       
+        float pitch = (pitchDueToPosition + pitchDueToControlThrow)/1.5f;// pitch can be calculate, 1.5 is changable, higher value more rotate
+        
+        float yaw = transform.localPosition.x * positionYawFactor;//different yaw when you move in x asix
+        float roll = xThrow * controlRollFactor;// roll with your joysick
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);// apply the change to che character
     }
 
     private void Move()
     {
-        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");//get data from CrossPlatformInputManager
         yThrow = CrossPlatformInputManager.GetAxis("Vertical");
        
         float xOffSet = xThrow * moveSpeed * Time.deltaTime;
         float yOffSet = yThrow * moveSpeed * Time.deltaTime;
-        float rowY = Mathf.Clamp(transform.localPosition.y + yOffSet, -2.5f, +2.5f);
-        float rowX = Mathf.Clamp(transform.localPosition.x + xOffSet, -3.23f, +3.23f);
+
+        float rowY = Mathf.Clamp(transform.localPosition.y + yOffSet, -2.5f, +2.5f);//limited the x y way can go
+        float rowX = Mathf.Clamp(transform.localPosition.x + xOffSet, -3.23f, +3.23f);//limited the x y way can go
         transform.localPosition = new Vector3(rowX, rowY, transform.localPosition.z);
     }
 
