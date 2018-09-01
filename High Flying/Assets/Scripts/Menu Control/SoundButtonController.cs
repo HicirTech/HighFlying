4e7e1aa68d43260278f39d74ced2b7f03d3cc011
Nicholas Jose <29719223+NZnick@users.noreply.github.com>
2018-09-01 19:05:37 +1200

    Designed Main Menu

---
+++ High Flying/Assets/Scripts/Menu Control/SoundButtonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundButtonController : MonoBehaviour {
+
+    [SerializeField]
+    private Button sourceButton;
+    [SerializeField]
+    private Sprite[] buttonImages = new Sprite[2];
+
+    public void onSoundPress()
+    {
+        changeButtonSprite();
+    }
+
+    private void changeButtonSprite()
+    {
+        if (sourceButton.image.sprite.Equals(buttonImages[0]))
+        {
+            sourceButton.image.sprite = buttonImages[1];
+        }
+        else
+        {
+            sourceButton.image.sprite = buttonImages[0];
+        }
+    }
+}
