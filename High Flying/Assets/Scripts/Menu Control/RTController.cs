ce3e9b7748b4d2653c54cfb78856ad064aafdac4
Nicholas Jose <29719223+NZnick@users.noreply.github.com>
2018-09-04 10:25:46 +1200

    Created Level Select Menu and Scripts

---
+++ High Flying/Assets/Scripts/Menu Control/RTController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RTController : MonoBehaviour {
+
+    private RenderTexture iceLevelRT;
+    private RenderTexture cityLevelRT;
+    [SerializeField]
+    private string iceLevelName = "Ice Level";
+    [SerializeField]
+    private string cityLevelName = "City Level";
+    [SerializeField]
+    private string relativePathToIceLevelRT = "RT/IceLevelRender";
+    [SerializeField]
+    private string relativePathToCityLevelRT = "RT/IceLevelRender";
+    [SerializeField]
+    private string iceRTCameraObjectName = "Ice Render Texture Camera";
+    [SerializeField]
+    private string cityRTCameraObjectName = "City Render Texture Camera";
+
+
+    public void Awake()
+    {
+        iceLevelRT = Resources.Load<RenderTexture>(relativePathToIceLevelRT);
+        cityLevelRT = Resources.Load<RenderTexture>(relativePathToCityLevelRT);
+
+        getLevel(iceLevelName);
+        getLevel(cityLevelName);
+        
+    }
+
+    public void Start()
+    {
+        getRTCameraInScene(cityRTCameraObjectName).Render();
+        getRTCameraInScene(iceRTCameraObjectName).Render();
+    }
+
+    private Scene getLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName, LoadSceneMode.Additive);
+         return SceneManager.GetSceneByName(levelName);
+    }
+
+    private Camera getRTCameraInScene(string RTCameraName)
+    {
+       GameObject cameraObject = GameObject.Find(RTCameraName);
+        return cameraObject.GetComponent<Camera>();
+    }
+}
---
+++ High Flying/Assets/Scripts/Menu Control/SwitchScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour {
+
+    [SerializeField]
+    private string iceLevelName = "Ice Level";
+    [SerializeField]
+    private string cityLevelName = "City Level";
+    [SerializeField]
+    private string mainMenuLevelName = "MainPlay";
+
+    public void loadIce()
+    {
+        SceneManager.LoadScene(iceLevelName);
+    }
+
+    public void loadCity()
+    {
+        SceneManager.LoadScene(cityLevelName);
+    }
+
+    public void loadMain()
+    {
+        SceneManager.LoadScene(mainMenuLevelName);
+    }
+}
