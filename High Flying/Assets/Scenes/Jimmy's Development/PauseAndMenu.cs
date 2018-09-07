5b1d71802c69135eef89cfd48c59f0f66882d4ec
Jimmy (My Phu Nguyen) <nphumy98@gmail.com>
2018-09-08 03:05:32 +1200

    pauseButton + pausePanel

---
+++ High Flying/Assets/Scenes/Jimmy's Development/PauseAndMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PauseAndMenu : MonoBehaviour {
+    public static bool pause;
+    public GameObject PauseMenuPanel;
+  
+	// Use this for initialization
+    void Start () {
+        pause = false;
+        PauseMenuPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void pauseAndResume()
+    {
+        pause = !pause;
+        if (pause==true)
+        {
+            Time.timeScale = 0;
+            PauseMenuPanel.SetActive(true);
+        }
+        else if (pause==false)
+        {
+            Time.timeScale = 1;
+            PauseMenuPanel.SetActive(false);
+        }
+    }
+}
