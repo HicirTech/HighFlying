d7bd3dc320df96f0fac81e52ed4d2562873ca165
Arniox <nikkdiehl@gmail.com>
2018-09-08 01:48:04 +1200

    Added point system
    - Started Tests for all 3 systems
    
    Point System:
    - Enabled toggle
    - Coins collected multiplier (leave at 1.75 otherwise huge numbers)
    - Rings passed multiplier (leave at 1.75 otherwise huge numbers)
    - Point ring multiplier (leave at 2 otherwise huge numbers)
    
    Added an enable toggle to all three systems for later pause mechanics

---
+++ High Flying/Assets/Scripts/NikkDev/PointSystem.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointSystem : MonoBehaviour {
+
+	[Tooltip("Toggle to enable or disable")][SerializeField]
+	public bool enable = true; //Variable used for pause functionality
+	[Tooltip("Number of points player has")][SerializeField]
+	public float points;
+	private Text pointsUI;
+
+	//score = ((Amount of coins collected in a mission * 1.75) * (Amount of total rings passed * 1.75)) * ((Point ring multiplier added up with each being 2)^difficulty rating)
+	
+	[Tooltip("This is the coins collected multiplier")][SerializeField][Range(0.0f, 5.0f)]
+	private float coinsCollMult = 1.75f;
+	[Tooltip("This is the rings passed multiplier")][SerializeField][Range(0.0f, 5.0f)]
+	private float ringsPassMult = 1.75f;
+	[Tooltip("This is the point ring multiplier")][SerializeField][Range(0.0f, 10.0f)]
+	private float pointRMult = 2.0f;
+	private int coinsCollectedCounter;
+	private int ringsPassedCounter;
+	private int pointRingCounter;
+ 	private int difficulty;
+
+	// Use this for initialization
+	void Start () {
+		//Initiate all variables
+		points = 1.0f;
+		coinsCollectedCounter = 0;
+		ringsPassedCounter = 0;
+		pointRingCounter = 0;
+
+		//Initiate the points text UI and fill
+		pointsUI = GameObject.Find("/IngameUI/Points").GetComponent<Text>();
+		pointsUI.text = "Points: "+points;
+
+		//Get difficulty rating
+		difficulty = GameObject.Find("/Character").GetComponent<HealthSystem>().difficultyRating;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//If script is enabled
+		if(enable){
+			calculatePoints();
+			changePointsValue();
+		}
+		else{
+			pointsUI.text = "";
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "CoinToCollect"){
+			coinsCollectedCounter++;
+			Debug.Log("Coin Collection - Collision Occured, coin collected counter increased to: "+coinsCollectedCounter);
+		}
+		if(col.gameObject.tag == "PointRing"){
+			pointRingCounter++;
+			Debug.Log("Point ring passed - Collision Occured, point ring counter increased to: "+pointRingCounter);
+		}
+		if(col.gameObject.tag == "PointRing" || col.gameObject.tag == "CoinRing" || col.gameObject.tag == "LifeRing"){
+			ringsPassedCounter++;
+			Debug.Log("Ring passed - Collision Occured, rings passed counter increased to: "+ringsPassedCounter);
+		}
+	}
+
+	void calculatePoints(){
+		points = Mathf.Round((((coinsCollectedCounter+1)*coinsCollMult)*(ringsPassedCounter*ringsPassMult))*Mathf.Pow((pointRingCounter*pointRMult), difficulty));
+		Debug.Log("Mathf.Round(((("+coinsCollectedCounter+"+1)*"+coinsCollMult+")"+
+				  "*("+ringsPassedCounter+"*"+ringsPassMult+"))"+
+				  "*Mathf.Pow(("+pointRingCounter+"*"+pointRMult+"),"+difficulty+"))");
+	}
+
+	void changePointsValue(){
+		pointsUI.text = "Points: "+points;
+	}
+
+}
---
+++ High Flying/Assets/Tests/Point_System_Test.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+/*public class Point_System_Test {
+
+    GameObject character;
+    PointSystem pSys;
+    int pointsBeforeEngage;
+    int pointsAfterEngage;
+
+
+
+    [Test]
+    public void Point_System_TestSimplePasses() {
+        // Use the Assert class to test conditions.
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator Point_System_TestWithEnumeratorPasses() {
+        character = GameObject.Find("/Character");
+        pSys = character.GetComponent<PointSystem>();
+        pointsBeforeEngage = pSys.points;    
+
+        yield return new WaitForSeconds(5);
+
+        pointsAfterEngage = pSys.points;
+
+        Assert.AreNotEqual(pointsBeforeEngage, pointsAfterEngage);
+    }
+}*/
