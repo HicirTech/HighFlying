2d50bf3b7b9b8c68fc78e95be4953486d03c2132
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-09-08 16:21:10 +1200

    Testing about music play and mute has been add
    It is must fail, because the script haven done yet.

---
+++ High Flying/Assets/Tests/MusicPlayerTest.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class MusicPlayerTest {
+
+    GameObject MusicPlayer;
+    
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator MuiscPlayerPlaysMusicTest() {
+        var musicPlayerPrefab = Resources.Load("");
+        MusicPlayer = GameObject.FindWithTag("MusicPlayer");
+        AudioSource testObject = MusicPlayer.GetComponent(typeof(AudioSource)) as AudioSource;
+    
+        testObject.Play();
+        yield return null;
+        Assert.Equals(testObject.isPlaying,true);
+    }
+
+    [UnityTest]
+    public IEnumerator MusicPlayerMuteTest()
+    {
+        var musicPlayerPrefab = Resources.Load("");
+        MusicPlayer = GameObject.FindWithTag("MusicPlayer");
+        AudioSource testObject = MusicPlayer.GetComponent(typeof(AudioSource)) as AudioSource;
+        yield return null;
+        testObject.Play();
+        Assert.Equals(testObject.isPlaying,true);
+        yield return null;// now music should play
+        
+        // some kind of mute method
+
+        Assert.Equals(testObject.isPlaying,false);
+        
+
+    }
+}
