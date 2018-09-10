0890423ca33c3ec8f4c845743ffa58e6bea43ae1
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-09-10 18:00:38 +1200

    About this commit
    <What's new>
    Timeline prototype added to the city level, no more way point system need
    new script makes character hit building will  sad "AHHHHHHHHHHHHHH"
    
    <need to fix>
    Some time you hit once building, will get off 2 life

---
+++ High Flying/Assets/Scenes/Zeting's development/HitBuildingHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBuildingHandler : MonoBehaviour {
+
+	// Use this for initialization
+
+	[SerializeField] AudioClip whenHit;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}	
+	void OnCollisionEnter(Collision col){
+	
+		print("hit");
+		AudioSource audio = gameObject.GetComponent<AudioSource>();
+		audio.PlayOneShot(this.whenHit);
+	
+	}
+}
+
