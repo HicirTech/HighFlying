0a9a2b8fa1e926e770352778c655d1d9ac3d4af3
Jimmy (My Phu Nguyen) <nphumy98@gmail.com>
2018-09-17 00:39:18 +1200

    modify Unit Test of Instruction Test to match the modification of instruction data

--- High Flying/Assets/Editor/Tests/InstructionTest.cs
+++ High Flying/Assets/Editor/Tests/InstructionTest.cs
@@ -7,11 +7,11 @@ public class InstructionTest
 {
     public class UpdateInstructionMethod
     {
-        [TestCase(1, "Can I play daddy : Player will have 5 lives, and a speed of 3.3m/s. Estimate time to finish: 5 minutes")]
+        [TestCase(1, "Can I play, Daddy: Player will have 5 lives, and a speed of 3.3m/s. Estimate time to finish: 5 minutes")]
         [TestCase(2, "Don't hurt me: player will have 4 lives, and a speed of 5.5m/s. Estimate time to finish: 3 minutes")]
         [TestCase(3, "Normal: Player will have 3 lives, and a speed of 8.5m/s. Estimate time to finish: 1.9 minutes")]
         [TestCase(4, "Sweaty Boy: Player will have 3 lives, and a speed of 20m/s. Estimate time to finish: 50 seconds")]
-        [TestCase(5, "Hell on Earth: Player will have 1 live, and a speed of 50m/s. Estimate time to finish: 20 seconds")]
+        [TestCase(5, "Hell on Earth: Player will have 1 life, and a speed of 50m/s. Estimate time to finish: 20 seconds")]
         [TestCase(-1, "")]
         public void UpdateDescriptionTest(float level, string expectedResult)
         {
