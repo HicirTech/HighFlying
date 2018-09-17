fab68792e20c67d1ab02621f5f0afe0c0241e669
NZnick <29719223+NZnick@users.noreply.github.com>
2018-09-18 08:55:54 +1200

    Rearanging Test Scripts

--- High Flying/Assets/Editor/Tests/PauseButtonTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.TestTools;
-using NUnit.Framework;
-using System.Collections;
-
-public class NewTestScript {
-
-    [Test]     public void PauseUnitTest() {         // Use the Assert class to test conditions.         UIBattleController UIButtons = new UIBattleController(); //set object for UIbatttleController          PauseAndMenu pauseMenu = UIButtons.getPauseAndMenu(); //get object from pauseMenu         Assert.AreEqual(0, pauseMenu.PauseGame()); //make sure time Scale return to 0 so the game pause         Assert.AreEqual(1, pauseMenu.ResumeGame()); //make sure time Scale return to 1 so the game resume     }
-}
