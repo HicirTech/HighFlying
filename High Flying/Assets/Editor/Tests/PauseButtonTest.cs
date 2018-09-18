00665dda7d0bfd18a4a4419a1b832f8218d21b19
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-09-18 12:31:53 +1200

    fix apple code

--- High Flying/Assets/Editor/Tests/PauseButtonTest.cs
+++ High Flying/Assets/Editor/Tests/PauseButtonTest.cs
@@ -5,5 +5,14 @@ using System.Collections;
 
 public class NewTestScript {
 
-    [Test]     public void PauseUnitTest() {         // Use the Assert class to test conditions.         UIBattleController UIButtons = new UIBattleController(); //set object for UIbatttleController          PauseAndMenu pauseMenu = UIButtons.getPauseAndMenu(); //get object from pauseMenu         Assert.AreEqual(0, pauseMenu.PauseGame()); //make sure time Scale return to 0 so the game pause         Assert.AreEqual(1, pauseMenu.ResumeGame()); //make sure time Scale return to 1 so the game resume     }
+    [Test]    
+    public void PauseUnitTest() {
+    // Use the Assert class to test conditions.
+    UIBattleController UIButtons = new UIBattleController();
+   // set object for UIbatttleController 
+    PauseAndMenu pauseMenu = UIButtons.getPauseAndMenu(); 
+    //get object from pauseMenu       
+     Assert.AreEqual(0, pauseMenu.PauseGame()); //make sure time Scale return to 0 so the game pause
+    Assert.AreEqual(1, pauseMenu.ResumeGame()); //make sure time Scale return to 1 so the game resume
+     }
 }
