5b4a9f3b0c560e961527d39e64c80433713943cd
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-09-18 12:17:32 +1200

    Revert "Rearanging Test Scripts"
    This reverts commit fab68792e20c67d1ab02621f5f0afe0c0241e669.

--- High Flying/Assets/Editor/Tests/MusicPlayerTest.cs
+++ High Flying/Assets/Editor/Tests/MusicPlayerTest.cs
@@ -3,23 +3,21 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
 
-public class MusicPlayerTest
-{
+public class MusicPlayerTest {
 
     GameObject MusicPlayer;
-
+    
     // A UnityTest behaves like a coroutine in PlayMode
     // and allows you to yield null to skip a frame in EditMode
     [UnityTest]
-    public IEnumerator MuiscPlayerPlaysMusicTest()
-    {
-        //    var musicPlayerPrefab = Resources.Load("");
+    public IEnumerator MuiscPlayerPlaysMusicTest() {
+    //    var musicPlayerPrefab = Resources.Load("");
         MusicPlayer = GameObject.FindWithTag("MusicPlayer");
         Assert.IsNotNull(MusicPlayer);
         AudioSource testObject = MusicPlayer.GetComponent(typeof(AudioSource)) as AudioSource;
         testObject.Play();
         yield return null;
-        Assert.Equals(testObject.isPlaying, true);
+        Assert.Equals(testObject.isPlaying,true);
     }
 
     [UnityTest]
@@ -30,13 +28,13 @@ public class MusicPlayerTest
         AudioSource testObject = MusicPlayer.GetComponent(typeof(AudioSource)) as AudioSource;
         yield return null;
         testObject.Play();
-        Assert.Equals(testObject.isPlaying, true);
+        Assert.Equals(testObject.isPlaying,true);
         yield return null;// now music should play
-
+        
         // some kind of mute method
 
-        Assert.Equals(testObject.isPlaying, false);
-
+        Assert.Equals(testObject.isPlaying,false);
+        
 
     }
 }
