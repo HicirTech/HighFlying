cde603e629c05ffe3a15f035e5d9bc4e2f6f04aa
Jimmy <wrk2544@autuni.ac.nz>
2018-09-28 02:36:13 +1200

    Revert "Merge branch 'Jimmy-dev'"
    This reverts commit b4b2ded3e5347b1d77ed7d0c01575c3742337639, reversing
    changes made to 01a29532298f52925e16688f8f4fdcf126829a3e.

--- High Flying/Assets/Scenes/Nikkolas' development/AccelerometerControl.cs
+++ High Flying/Assets/Scenes/Nikkolas' development/AccelerometerControl.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AccelerometerControl : MonoBehaviour {
-<<<<<<< HEAD
 
 	[Tooltip("Drag and drop the character here")][SerializeField]
 	GameObject character;
@@ -11,10 +10,5 @@ public class AccelerometerControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		character.transform.Translate(Input.acceleration.x, -Input.acceleration.z, 0);
-=======
-	// Update is called once per frame
-	void Update () {
-		transform.Translate(Input.acceleration.x, Input.acceleration.y, 0);
->>>>>>> Jimmy-dev
 	}
 }
