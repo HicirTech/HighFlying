591403462a4961a040740d7662c1b744a86c2e40
Jimmy <nphumy98@gmail.com>
2018-09-27 20:45:15 +0700

    finish TDD

---
+++ High Flying/Assets/Editor/Tests/EndGameInfoTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.TestTools;
+
+public class EndGameInfoTest
+{
+    
+    public const string scenePath = "Assets/Scenes/CityLevel/CityLevel.unity";
+    [UnityTest]
+    public IEnumerator EndGameInfo()
+    {
+        var sceneAsync = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
+        while (!sceneAsync.isLoaded)
+        {
+            yield return null;
+        }
+        var uiGameOver = GameObject.Find("IngameUI").GetComponentInChildren<GameOver>(true);
+        Assert.That(uiGameOver != null, "Can't find GameOver");
+
+        uiGameOver.LevelComplete(1, 1, 1, 1, 1);
+
+        var textList = uiGameOver.gameObject.GetComponentsInChildren<Text>(true);
+        Text textCoinCollected = null;
+        foreach (Text txt in textList)
+        {
+            if (txt.name == "NumberCoins")
+            {
+                textCoinCollected = txt;
+                break;
+            }
+        }
+        Assert.That(textCoinCollected.text == "1/20", "Result is wrong! Good result is 1/20");
+    }
+}
---
+++ High Flying/Assets/Editor/Tests/PlayAgainTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.TestTools;
+
+public class PlayAgainTest
+{
+    public const string scenePath = "Assets/Scenes/CityLevel/CityLevel.unity";
+    [UnityTest]
+    public IEnumerator PlayAgainButtonTest()
+    {
+        var sceneAsync = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
+        var sceneAsyncName = sceneAsync.name;
+        Scene scenePlayAgain = new Scene();
+        while (!sceneAsync.isLoaded)
+        {
+            yield return null;
+        }
+        var buttonList = GameObject.Find("IngameUI").GetComponentsInChildren<Button>(true);
+        Button buttonPlayAgain = null;
+        foreach (Button btn in buttonList)
+        {
+            if (btn.name == "BackToPlayAgainButton")
+            {
+                buttonPlayAgain = btn;
+                buttonPlayAgain.onClick.AddListener(() =>
+                {
+                    scenePlayAgain = EditorSceneManager.OpenScene(scenePath);
+                    Debug.Log(sceneAsyncName + " " + scenePlayAgain.name);
+                    Assert.That(sceneAsyncName == scenePlayAgain.name, "Play again not the same scene");
+                });
+            }
+        }
+        
+        Assert.That(buttonPlayAgain != null, "Can't find Button Play Again");
+        buttonPlayAgain.onClick.Invoke();
+        
+    }
+}
