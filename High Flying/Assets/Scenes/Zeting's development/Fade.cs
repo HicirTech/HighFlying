e359acc4aae25b661061660df562ab8e98fce7b6
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-10-04 11:33:06 +1300

    fade objects in ice level
    🙃

--- High Flying/Assets/Scenes/Zeting's development/Fade.cs
+++ High Flying/Assets/Scenes/Zeting's development/Fade.cs
@@ -6,17 +6,37 @@ public class Fade : MonoBehaviour {
 
 	// Use this for initialization
    [Tooltip("How fast the object fade")] [SerializeField] private float fadeSpeed = 2.5f;
-
-    /// <summary>
-    /// a fade function will fade a object, use message call from outside
-    /// </summary>
-    public void fade()
+    private bool fadable = false;
+    
+    
+    private void Update()
     {
-         gameObject.GetComponent<Renderer>().material.color= new Color(
+        this.fade();
+    }
+    private void OnCollisionEnter(Collision e)
+    {
+        this.fadable=true;
+    }
+
+    private void OnTriggerEnter(Collider e)
+    {
+        this.fadable=true;
+        print("T");
+    }
+    
+    /// <summary>
+    /// a fade function will fade a object
+    /// </summary>
+    private void fade()
+    {
+        if(fadable)
+        {gameObject.GetComponent<Renderer>().material.color= new Color(
             gameObject.GetComponent<Renderer>().material.color.r,
              gameObject.GetComponent<Renderer>().material.color.g,
               gameObject.GetComponent<Renderer>().material.color.b
               , Mathf.Clamp(gameObject.GetComponent<Renderer>().material.color.a -
                (fadeSpeed* Time.deltaTime),0.00f,1.0f));
-     }
+            print("fade");}
+       
+    }
 }
