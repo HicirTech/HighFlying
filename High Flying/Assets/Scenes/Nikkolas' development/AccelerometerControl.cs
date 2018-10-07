7242193876a20daf204eabcfe92c7eac34728554
Arniox <nikkdiehl@gmail.com>
2018-10-07 20:22:29 +1300

    Sortof finally fixed accelerometer.
     - Made some collider walls to the left and right of the character. These are invisable
    
     - if the character collides with either of them, his acclerometer control is frozen and only unfrozen when he moves in the oposite direction of the wall.
    
     - Small bug to note: If you are sitting on the invisable wall and move a very small amount away and then quickly back in and repeat this, you can phase through the wall due to collisions in unity not being a consant check and only activated when collision occures.

--- High Flying/Assets/Scenes/Nikkolas' development/AccelerometerControl.cs
+++ High Flying/Assets/Scenes/Nikkolas' development/AccelerometerControl.cs
@@ -5,14 +5,36 @@ using UnityEngine;
 public class AccelerometerControl : MonoBehaviour {
 
 	private float accel;
+	private bool stop = false;
+	private string sideCheck;
+	[SerializeField] private TextMesh debugText;
 
 	// Update is called once per frame
 	void Update () {
 		float accel = Input.acceleration.x;
-		transform.Translate(accel, 0, 0);
+		if(!stop){
+			transform.Translate(accel, 0, 0);
+		}else{
+			switch(sideCheck){
+				case("right"):
+					if(accel < 0) stop = false;
+					break;
+				case("left"):
+					if(accel > 0) stop = false;
+					break;
+				default:
+					break;
+			}
+		}
+
+		debugText.text = "Acceleration: "+accel+"\nX Position: "+transform.position.x+"\nY Position: "+transform.position.y+"\nFrozen: "+stop;
 	}
 
 	void OnCollisionEnter(Collision col){
-		if(col.gameObject.tag=="ColliderWall") accel = 0;
+		//if collide, stop
+		if(col.gameObject.tag=="ColliderWall") stop = true;accel=0;
+		//get collider name
+		if(col.gameObject.name == "right" || col.gameObject.name == "left") sideCheck = col.gameObject.name;
+		
 	}
 }
