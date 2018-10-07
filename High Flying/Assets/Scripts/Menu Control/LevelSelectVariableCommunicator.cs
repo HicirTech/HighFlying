eb960f90695533127653e9aadcfa74fb022da990
NZnick <29719223+NZnick@users.noreply.github.com>
2018-10-07 21:53:57 +1300

    Made Varibale Container, Communicator and Tests

---
+++ High Flying/Assets/Scripts/Menu Control/LevelSelectVariableCommunicator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectVariableCommunicator : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+        Slider theDifficultySlider = FindObjectOfType<Slider>();
+
+        sendNewDifficultyRating(theDifficultySlider.value);
+	}
+	
+	
+	public void sendNewDifficultyRating (float value)
+    {
+        VariableContainer thePasser = FindObjectOfType<VariableContainer>();
+        int roundedValue = Mathf.RoundToInt(value);
+        thePasser.difficultyRating = roundedValue;
+	}
+}
---
+++ High Flying/Assets/Scripts/Menu Control/VariableContainer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VariableContainer : MonoBehaviour
+{
+
+
+    private int objectAge = 0;
+    public int difficultyRating = 0;
+
+    //void OnEnable()
+    //{
+    //    //Make sure that the 'OnSceneLoaded' function is subscribed to the sceneLoaded event when the script is enabled.
+    //    SceneManager.sceneLoaded += OnSceneLoaded;
+    //}
+
+    //void OnDisable()
+    //{
+    //    //Make sure that the 'OnSceneLoaded' function is unsubscribed to the sceneLoaded event when the script is disabled.
+    //    SceneManager.sceneLoaded -= OnSceneLoaded;
+    //}
+
+    //void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    //{
+    //    VariableContainer[] containersFound = GameObject.FindObjectsOfType<VariableContainer>();
+    //    VariableContainer survivingObject = destroyYoungestDuplicate(containersFound);
+    //    DontDestroyOnLoad(survivingObject.gameObject);
+    //    survivingObject.objectAge++;
+
+    //}
+
+    //VariableContainer destroyYoungestDuplicate(VariableContainer[] presentObjects)
+    //{
+
+    //    if (presentObjects.Length > 1)
+    //    {
+    //        foreach (VariableContainer currentObject in presentObjects)
+    //        {
+    //            if (currentObject.objectAge == 0)
+    //            {
+    //                Destroy(currentObject);
+    //            }
+    //        }
+
+    //    }
+
+    //    VariableContainer survivingObject = GameObject.FindObjectOfType<VariableContainer>();
+    //    survivingObject.objectAge = 0;
+
+    //    return survivingObject;
+    //}
+}
