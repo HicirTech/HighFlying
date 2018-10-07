942a4a25fdb1d0f92c1f120124837e89f3ecc6ae
NZnick <29719223+NZnick@users.noreply.github.com>
2018-10-08 09:36:56 +1300

    Automatic Unity Chnages

--- High Flying/Assets/Scripts/Menu Control/VariableContainer.cs
+++ High Flying/Assets/Scripts/Menu Control/VariableContainer.cs
@@ -10,45 +10,45 @@ public class VariableContainer : MonoBehaviour
     private int objectAge = 0;
     public int difficultyRating = 0;
 
-    //void OnEnable()
-    //{
-    //    //Make sure that the 'OnSceneLoaded' function is subscribed to the sceneLoaded event when the script is enabled.
-    //    SceneManager.sceneLoaded += OnSceneLoaded;
-    //}
+    void OnEnable()
+    {
+        //Make sure that the 'OnSceneLoaded' function is subscribed to the sceneLoaded event when the script is enabled.
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
 
-    //void OnDisable()
-    //{
-    //    //Make sure that the 'OnSceneLoaded' function is unsubscribed to the sceneLoaded event when the script is disabled.
-    //    SceneManager.sceneLoaded -= OnSceneLoaded;
-    //}
+    void OnDisable()
+    {
+        //Make sure that the 'OnSceneLoaded' function is unsubscribed to the sceneLoaded event when the script is disabled.
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
 
-    //void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    //{
-    //    VariableContainer[] containersFound = GameObject.FindObjectsOfType<VariableContainer>();
-    //    VariableContainer survivingObject = destroyYoungestDuplicate(containersFound);
-    //    DontDestroyOnLoad(survivingObject.gameObject);
-    //    survivingObject.objectAge++;
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        VariableContainer[] containersFound = GameObject.FindObjectsOfType<VariableContainer>();
+        VariableContainer survivingObject = destroyYoungestDuplicate(containersFound);
+        DontDestroyOnLoad(survivingObject.gameObject);
+        survivingObject.objectAge++;
 
-    //}
+    }
 
-    //VariableContainer destroyYoungestDuplicate(VariableContainer[] presentObjects)
-    //{
+    VariableContainer destroyYoungestDuplicate(VariableContainer[] presentObjects)
+    {
 
-    //    if (presentObjects.Length > 1)
-    //    {
-    //        foreach (VariableContainer currentObject in presentObjects)
-    //        {
-    //            if (currentObject.objectAge == 0)
-    //            {
-    //                Destroy(currentObject);
-    //            }
-    //        }
+        if (presentObjects.Length > 1)
+        {
+            foreach (VariableContainer currentObject in presentObjects)
+            {
+                if (currentObject.objectAge == 0)
+                {
+                    Destroy(currentObject);
+                }
+            }
 
-    //    }
+        }
 
-    //    VariableContainer survivingObject = GameObject.FindObjectOfType<VariableContainer>();
-    //    survivingObject.objectAge = 0;
+        VariableContainer survivingObject = GameObject.FindObjectOfType<VariableContainer>();
+        survivingObject.objectAge = 0;
 
-    //    return survivingObject;
-    //}
+        return survivingObject;
+    }
 }
