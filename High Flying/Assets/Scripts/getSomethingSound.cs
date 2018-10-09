b04561db4117046a7344d9b566bdc144e6ab2b27
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-10-09 15:26:25 +1300

    rings and coin collection sound has beed add to both level

---
+++ High Flying/Assets/Scripts/getSomethingSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getSomethingSound : MonoBehaviour {
+	AudioSource source;
+	[SerializeField]AudioClip clip;
+	private void OnCollisionEnter(Collision collision)
+    {
+		source = this.gameObject.AddComponent<AudioSource>();
+		source.playOnAwake=false;
+        print("C");
+		AudioListener.volume=1;
+		source.PlayOneShot(clip);
+		
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+		source = this.gameObject.AddComponent<AudioSource>();
+		source.playOnAwake=false;
+		print("T");
+		AudioListener.volume=1;
+		source.PlayOneShot(clip);
+    }
+	// Use this for initialization
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
